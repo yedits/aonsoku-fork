c44ec26bf1f14e0f4c3731396a4ce38c70e58897
onyxdagoat1 <aecloud2004@gmail.com>
2025-12-26 12:45:16 +0100

    Update logo: use yedits-YE-logo-colour-old1.webp

--- src/app/components/app-icon.tsx
+++ src/app/components/app-icon.tsx
@@ -8,7 +8,7 @@ type AppIconProps = Omit<SVGProps<SVGImageElement>, 'width' | 'height'> & {
 export function AppIcon({ size = 48, className, ...props }: AppIconProps) {
   return (
     <img
-      src="/resources/icons/yedits-YE-logo-colour.webp"
+      src="/resources/icons/yedits-YE-logo-colour-old1.webp"
       alt="yedits.net"
       width={size}
       height={size}
